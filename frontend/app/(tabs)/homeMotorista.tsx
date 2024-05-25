b847819d0cf4ca7f84128ab3200227b736c67e9b
flavioaquino <flabas@ufpi.edu.br>
2024-05-25 12:42:33 -0300

    Tela de login e tela de passageiro

--- frontend/app/(tabs)/homeMotorista.tsx
+++ frontend/app/(tabs)/homeMotorista.tsx
@@ -1,21 +1,20 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { Button, Input } from 'react-native-elements';
-import { Image } from 'react-native'; // Import the Image component from react-native
+import { Button } from 'react-native-elements';
 
 export default function HomeScreen() {
   return (
-
     <View style={styles.container}>
       <View style={styles.titleContainer}>
-        <Image source={require('@/assets/images/boraja_logo.jpg')} style={{ alignSelf: 'center', width: 230, height: 60 }} />
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>Faça o login</Text>
+        <Text style={styles.title}>Início</Text>
       </View>
       <View style={styles.buttonContainer}>
-        <Input placeholder='E-mail' />
-        <Input placeholder='Senha' />
-        <Button title="Login" onPress={() => console.log("Button 4 pressed")} buttonStyle={styles.buttonStyle2} />
+        <Button title="Buscar Viagem" onPress={() => console.log("Button 1 pressed")} buttonStyle={styles.buttonStyle1} />
+        <View style={styles.buttonSpacer} /> 
+        <Button title="Iniciar Viagem" onPress={() => console.log("Button 2 pressed")} buttonStyle={styles.buttonStyle2} />
+        <View style={styles.buttonSpacer} /> 
+        <Button title="Minhas Viagens" onPress={() => console.log("Button 3 pressed")} buttonStyle={styles.buttonStyle1} />
+        <View style={styles.buttonSpacer} /> 
+        <Button title="Meu Saldo" onPress={() => console.log("Button 4 pressed")} buttonStyle={styles.buttonStyle2} />
       </View>
     </View>
   );
@@ -33,14 +32,7 @@ const styles = StyleSheet.create({
     top: 0, // Fixa no topo
     width: '100%',
     alignItems: 'center',
-    marginTop: 80, // Distância do topo
-  },
-  textContainer: {
-    position: 'absolute', // Posiciona o título absolutamente
-    top: 0, // Fixa no topo
-    width: '100%',
-    alignItems: 'center',
-    marginTop: 180, // Distância do topo
+    marginTop: 50, // Distância do topo
   },
   title: {
     fontSize: 24, // Tamanho do título
@@ -51,6 +43,11 @@ const styles = StyleSheet.create({
     width: '80%', // Define a largura do container de botões
     justifyContent: 'center', // Centraliza verticalmente
     marginTop: 100, // Espaçamento do topo para os botões
+  },
+  buttonStyle1: {
+    height: 50, // Define a altura dos botões
+    backgroundColor: 'black', // Cor de fundo dos botões
+    borderRadius: 30, // Borda arredondada
   },
   buttonStyle2: {
     height: 50, // Define a altura dos botões
