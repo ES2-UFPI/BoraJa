7caa7169e962d9635b31d12b0448fefc3b0fb91a
Heitor Andrade Moura <heitor.moura@etipi.pi.gov.br>
2024-05-27 01:30:52 -0300

    💄 Melhorando estilização da tela de login

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,63 +2,57 @@ import React, { useState, useContext } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import { Image } from 'react-native';
-import { AuthContext } from './AuthProvider'; // Importa o AuthContext
-import { useRouter } from 'expo-router'; // Importa useRouter do Expo Router
-import { color } from 'react-native-elements/dist/helpers';
+import { AuthContext } from './AuthProvider';
+import { useRouter } from 'expo-router';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function LoginScreen() {
-  const { login } = useContext(AuthContext); // Usa o contexto de autenticação
-  const router = useRouter(); // Obtém a função de redirecionamento
+  const { login } = useContext(AuthContext);
+  const router = useRouter();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
 
   const handleLogin = async () => {
-    try {
-      // Enviar solicitação POST para o endpoint de login
-      // const response = await fetch('http://localhost:8000/login/', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({ email, password }),
-      // });
+    if (!email || !password) {
+      setError('Por favor, preencha todos os campos.');
+      return;
+    }
 
-      // // Verificar se a resposta do servidor é bem-sucedida
-      // if (response.ok) {
-      //   router.push('screens/home');
-      if (password == '1234' && email == 'admin') {
-        router.push('screens/home'); // Redireciona para a tela inicial
+    try {
+      if (password === '1234' && email === 'admin') {
+        router.push('screens/home');
       } else {
-        // Se a resposta do servidor não for bem-sucedida, exiba uma mensagem de erro
-        console.error('Erro ao fazer login:');
+        setError('E-mail ou senha incorretos.');
       }
     } catch (error) {
-      // Se ocorrer um erro durante a solicitação, exiba uma mensagem de erro
       console.error('Erro ao fazer login:', (error as Error).message);
+      setError('Erro ao fazer login. Tente novamente.');
     }
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
-        <Image source={require('@/assets/images/boraja_logo.jpg')} style={{ alignSelf: 'center', width: 230, height: 60 }} />
+        <Image source={require('@/assets/images/boraja_logo.jpg')} style={{ alignSelf: 'center', width: 250, height: 60 }} />
       </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>Faça o login</Text>
-      </View>
+  
       <View style={styles.inputContainer}>
         <Input
           placeholder='E-mail'
           onChangeText={(text) => setEmail(text)}
           value={email}
+          leftIcon={<Icon name='envelope' size={18} color='#999' />}
+          leftIconContainerStyle={{ marginRight: 10 }}
         />
         <Input
           placeholder='Senha'
           onChangeText={(text) => setPassword(text)}
           value={password}
           secureTextEntry
+          leftIcon={<Icon name='lock' size={24} color='#999' />}
+          leftIconContainerStyle={{ marginRight: 10 }}
         />
         {error && <Text style={styles.errorText}>{error}</Text>}
         <Button
@@ -67,7 +61,7 @@ export default function LoginScreen() {
           buttonStyle={styles.buttonStyle}
         />
         <View style={styles.textContainer}>
-          <Text onPress={() => router.push('screens/cadastro')}>Não tem cadastro ainda? <Text style={styles.cadastrarText}>Cadastrar</Text></Text>
+          <Text onPress={() => router.push('screens/cadastro')}>Não tem cadastro ainda? <Text style={styles.registerText}>Cadastrar</Text></Text>
         </View>
       </View>
     </View>
@@ -98,17 +92,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  cadastrarText: {
+  registerText: {
     color: '#5271FF',
   },
   inputContainer: {
     width: '80%',
-    marginTop: 80,
+    marginTop: 70,
   },
   buttonStyle: {
     height: 50,
     backgroundColor: '#F3AC3D',
-    borderRadius: 30,
+    borderRadius: 12,
+    marginTop: 15,
   },
   errorText: {
     color: 'red',
